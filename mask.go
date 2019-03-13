165e2422c64f00c5e646562b3c4e351e5258bc3c
Tamura Yukichi <tamura.yukichi@gmail.com>
2019-03-13 17:05:56 +0900

    Refactor

--- mask.go
+++ mask.go
@@ -20,7 +20,7 @@ var (
 	defaultMaskFunc = MaskFunc
 )
 
-// MaskWithFunc masks the given json string
+// Mask masks the given json string
 func Mask(jsonString string) (string, error) {
 	return MaskWithFunc(jsonString, defaultMaskFunc)
 }
@@ -35,9 +35,7 @@ func MaskWithFunc(jsonString string, maskFunc func(s string) string) (string, er
 		return "", err
 	}
 
-	for k, v := range before {
-		mask(k, v, after, maskFunc)
-	}
+	mask(before, after, maskFunc)
 
 	b, err := json.Marshal(after)
 	if err != nil {
@@ -47,28 +45,39 @@ func MaskWithFunc(jsonString string, maskFunc func(s string) string) (string, er
 	return string(b), nil
 }
 
-func mask(key string, val interface{}, obj map[string]interface{}, callback func(s string) string) {
+func mask(before, after map[string]interface{}, callback func(s string) string) {
+	for k, v := range before {
+		recursiveMask(k, v, after, callback)
+	}
+}
+
+func recursiveMask(key string, val interface{}, obj map[string]interface{}, callback func(s string) string) {
+
 	switch value := val.(type) {
+
 	case map[string]interface{}:
 		ret := make(map[string]interface{})
 		for k, v := range value {
-			mask(k, v, ret, callback)
+			recursiveMask(k, v, ret, callback)
 		}
 		obj[key] = ret
+
 	case []interface{}:
 		ret := make(map[string]interface{})
 		for k, v := range value {
 			i := strconv.Itoa(k)
-			mask(i, v, ret, callback)
+			recursiveMask(i, v, ret, callback)
 		}
 		slice := make([]interface{}, 0)
 		for _, v := range ret {
 			slice = append(slice, v)
 		}
 		obj[key] = slice
+
 	case string:
 		// mask the string value
 		obj[key] = callback(value)
+
 	default:
 		obj[key] = val
 	}
